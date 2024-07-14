86adaf643faa62b1bfcc83159f929f9306019ac9
Emiliano Landi <Emilianolandi75@gmail.com>
2024-07-14 15:12:26 -0300

    boton proyect

--- component/introduccion.tsx
+++ component/introduccion.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from "next/image";
+import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
 
 
@@ -25,24 +26,44 @@ const Introduction = () =>{
   <br />
  
   
-    Si puedes pensarlo, <br />
+    Si puedes pensarlo, 
+    <br />
           <TypeAnimation
             sequence={[
               'puedes programarlo',
-              800,
+              2000,
               'puedes optimizarlo',
-              1080,
+              2000,
               'puedes implementarlo',
-              2300,
+              2000,
               'puedes desarrollarlo',
-             7000
+             2000
             ]}
             wrapper="span"
             speed={70}
             repeat={Infinity}
             className="font-bold text-secondary"
           />
+        <p className='Impresion'>
+          <br />
+          <br />
+          como desarrollador frontend y creador de contenido me dedico a convina
+          diseño
+          <br />
+           y funcionalidad para crear experiencias digitales
+          impactante y accesibles
+        </p>
+        {/* flex item-center justify-center gap-3 md:justify-start md:gap-10 */}
+        <div className='Contenedor-boton'>
+
+          <Link href="/>projects" className='Boton' >
+          {/* px-3 py-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50 */}
+          Ver proyectos
+          </Link>
+       
+        </div>
         </h2>
+        
       </div>
         </div>
     </div>
