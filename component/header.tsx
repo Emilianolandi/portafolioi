d46d8bfd0c790d2e3ac8528a1127c57734c57369
Emiliano Landi <Emilianolandi75@gmail.com>
2024-07-14 03:45:03 -0300

    ordenando informacion personal

--- component/header.tsx
+++ component/header.tsx
@@ -12,6 +12,7 @@ const Header = () => {
         
         <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10">
             <header>
+           
                 <div className="container justify-between max-w-13xl mx-auto md:flex">
                     <Link href='/'>
                         
