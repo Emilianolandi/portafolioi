65e20114d74c315ddfb7eee2be9d15330404d3b6
Emiliano Landi <Emilianolandi75@gmail.com>
2024-09-26 18:58:39 -0300

    slider

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -16,10 +16,10 @@ const ServicesPages = () => {
                     <h1 className="Service-Title">Mis <span className="font-bold text-secondary"> servicios</span></h1>
                     <p className="Service-Info">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
                     <button className="px-3 py-2 rounded-lg bg-secondary opacity-75 hover:bg-secondary/95">Contacta conmigo</button>
+            </div>
 
                 {/* SLIDER */}
                 
-            </div>
                     <SliderServices />
                 </div>
                  
