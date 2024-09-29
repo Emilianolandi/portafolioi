5f635cf9cff98d4272a0e2dcb74cd330cf3f4c03
Emiliano Landi <Emilianolandi75@gmail.com>
2024-09-29 17:32:59 -0300

    posicionamiento de boton servicios

--- data.tsx
+++ data.tsx
@@ -67,29 +67,37 @@ export const dataAboutPage = [
         title: "Frontend Developer",
         subtitle: "TechSolutions",
         description: "Colabora con un equipo dinámico para desarrollar interfaces de usuario atractivas y funcionales que impulsen el éxito de nuestros clientes en el mundo digital.",
-        date: "Nov 2023 ",
+        date:"Nov 2023 ",
     },
     {
         id: 2,
         title: "Creador de Experiencias Digitales",
         subtitle: "PixelCrafters",
         description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
-        date: "May 2021",
+        date:"May 2021",
     },
     {
         id: 3,
         title: "Especialista en Desarrollo Frontend",
         subtitle: "CodeForge Solutions",
         description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
-        date: "Ago 2019",
+        date:"Ago 2019",
     },
     {
         id: 4,
-        title: "Prácticas Grado",
-        subtitle: "WebWizards Inc.",
-        description: "Únete a nosotros mientras creamos sitios web y aplicaciones interactivas que sorprenden y deleitan a nuestros clientes. Si tienes pasión por el diseño y la programación, y disfrutas colaborar en un entorno creativo, ¡queremos conocerte!        ",
-        date: "Mar 2018",
-    },
+        title: "Especialista en Desarrollo Frontend",
+        subtitle: "CodeForge Solutions",
+        description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
+        date:"Ago 2019",
+    }
+//   
+//     {
+//         id: 4,
+//         title: "Prácticas Grado",
+//         subtitle: "WebWizards Inc.",
+//         description: "Únete a nosotros mientras creamos sitios web y aplicaciones interactivas que sorprenden y deleitan a nuestros clientes. Si tienes pasión por el diseño y la programación, y disfrutas colaborar en un entorno creativo, ¡queremos conocerte!        ",
+//         date:"Mar 2018",
+//     },
 ]
 
 export const dataCounter = [
