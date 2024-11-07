fb1661b82e7daf076fecb6fef9538b7304b21ce8
Emiliano Landi <Emilianolandi75@gmail.com>
2024-11-07 19:54:29 -0300

    tarjetas de portfolio

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -1,7 +1,10 @@
 import AvatarPortfolio from "@/component/avatar-porfolio";
+import CircleImage from "@/component/circle-image";
 import ContainerPage from "@/component/conteiner";
 import CoverParticles from "@/component/cover-particles";
+import PortfolioBox from "@/component/portfolio-box";
 import TransitionPage from "@/component/transition-pages";
+import { dataPortfolio } from "@/data";
 
 
 
@@ -14,7 +17,20 @@ const PortfolioPage = () => {
        <CoverParticles />
         <ContainerPage>
         <AvatarPortfolio />
+        <CircleImage/>
 
+        <div className="flex flex-col justify-center h-full">
+        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
+        Proyectos 
+            <span className="text-secondary font-bold">  Realizados</span>
+            </h1>
+
+            <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
+        {dataPortfolio.map((data)=>(
+        <PortfolioBox key={data.id} data={data}/>
+    ))}
+            </div>
+        </div>
        </ContainerPage>
         </>
     );
--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -16,7 +16,13 @@ const ServicesPages = () => {
                <div className="Service-Padre ">
                     <h1 className="Service-Title mt-20">Mis <span className="font-bold text-secondary"> servicios</span></h1>
                     <p className="Service-Info">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
-                    <button className="px-3 py-2 ml-24 rounded-lg bg-secondary opacity-75 hover:bg-secondary/95">Contacta conmigo</button>
+                  
+                    <a href="mailto:EmilianoLandi.dev@gmail.com">
+  <button className="px-3 py-2 ml-24 rounded-lg bg-secondary opacity-75 hover:bg-secondary/95">
+    Contacta conmigo
+  </button>
+</a>
+
                 </div> 
                 <SliderServices />
                  </div>
