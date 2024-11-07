fe45ec97a77797200f6beaaca098adb0405149f7
Emiliano Landi <Emilianolandi75@gmail.com>
2024-11-07 12:24:04 -0300

    posicionando elementos de mi trayectoria

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -8,13 +8,13 @@ import TransitionPage from "@/component/transition-pages";
 const ServicesPages = () => {
     return (
         <>
-            <TransitionPage />
-            <CoverParticles />
            
 
-<div className="Page-service ">
-               <div className="Service-Padre">
-                    <h1 className="Service-Title">Mis <span className="font-bold text-secondary"> servicios</span></h1>
+            <TransitionPage />
+            <CoverParticles />
+<div className="Page-service mt-20">
+               <div className="Service-Padre ">
+                    <h1 className="Service-Title ">Mis <span className="font-bold text-secondary"> servicios</span></h1>
                     <p className="Service-Info">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
                     <button className="px-3 py-2 ml-24 rounded-lg bg-secondary opacity-75 hover:bg-secondary/95">Contacta conmigo</button>
                 </div> 
