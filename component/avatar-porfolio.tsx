18903b65e0380f8c0bc250a7489d946b083518d9
Emiliano Landi <Emilianolandi75@gmail.com>
2025-02-01 14:06:59 -0300

    cambios de imagen

--- component/avatar-porfolio.tsx
+++ component/avatar-porfolio.tsx
@@ -7,7 +7,7 @@ import MotionTransition from "./transition-component";
 const AvatarPortfolio = () => {
     return (
         <MotionTransition position='bottom' className="Avatar-p">
-        <Image src="/avatar-works.png" width="250" height="250" alt="Particles " />
+        <Image src="/trayectoria.jpg" width="250" height="250" alt="Particles " />
     </MotionTransition>
     );
 }
