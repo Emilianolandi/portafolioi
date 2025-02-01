13ffa71bfb264e72a863ccbf530e5817154c6831
Emiliano Landi <Emilianolandi75@gmail.com>
2025-02-01 17:56:36 -0300

    cambio de imagen de avatar

--- component/avatar.tsx
+++ component/avatar.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export function Avatar() {
     return (
         <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute">
-        <Image src="/" width={400} height={400} className="Img-About" alt="Particles" />
+        <Image src="/mis-certificaciones.png" width={380} height={400} className="Img-About" alt="Particles" />
     </MotionTransition>
     
     )
